e3f1e268af1a4196f36ede189e29c3c540920fee
Carmen <ca.santiago.me@gmail.com>
2023-12-06 20:12:32 -0600

    feat: get habits by id

--- src/routes/habits.ts
+++ src/routes/habits.ts
@@ -4,8 +4,17 @@ import {
   createHabitSchema,
 } from "../controllers/habits/create";
 import { failActionHandler } from "../helper/validation";
+import { GetHabitsByUserIdController } from "../controllers/habits/getByUserId";
 
 export const HabitsRouter: ServerRoute[] = [
+  {
+    method: "GET",
+    path: "/habits",
+    options: {
+      auth: "auth0",
+    },
+    handler: GetHabitsByUserIdController,
+  },
   {
     method: "POST",
     path: "/habits",
