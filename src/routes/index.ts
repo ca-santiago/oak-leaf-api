55e37a9b07b6b0a8c39547dfc8e6c98d4206e656
Carmen <ca.santiago.me@gmail.com>
2023-12-11 16:29:29 -0600

    feat: ping pong route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,13 @@ import { HabitsRouter } from "./habits";
 import { getIncidencesRoutes } from "./incidences";
 
 export const loadRoutes = (server: Server): Server => {
+  server.route({
+    method: "GET",
+    path: "/ping",
+    handler: (request, h) => {
+      return "pong";
+    },
+  });
   server.route(HabitsRouter);
   server.route(getIncidencesRoutes("/incidences"));
   return server;
